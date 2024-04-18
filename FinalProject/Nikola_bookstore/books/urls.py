c12fb76d872262133579f0bbd5e769c54ac02678
Niki9526 <n01591561@humber.ca>
2024-04-18 11:22:42 -0400

    created url path for books app

--- FinalProject/Nikola_bookstore/books/urls.py
+++ FinalProject/Nikola_bookstore/books/urls.py
@@ -2,8 +2,26 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+#_homepage
+#_detail
 urlpatterns = [
-   path ('', views.book, name="books"),
-   path('book/', views.book, name="book"),
-   path ('books/<str:pk>/', views.books, name="books" )
+   path ('', views.books, name="books"), # all books BOOKS
+   #path('book/', views.books, name="books"),
+   path ('book/<str:pk>/', views.book, name="book" ) ,# detial book one book BOOK
+   
+    # Route to add book using form
+   path ('addbook/', views.addBook, name='addBook'),
+   
+   # Route to edit book using form
+    path('editbook/<str:pk>/', views.editBook, name="editBook"),
+    
+     # Route to delete book
+    path ('deletebook/<str:pk>/', views.deleteBook, name="deleteBook"),
+    
 ]
+
+    
+   
+   
+  
+
